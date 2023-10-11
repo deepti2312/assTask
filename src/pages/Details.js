30d2ff2a4bf6160c84467d7a97e1f1b57d6d633f
deepti2312 <devishwakarma@bestpeers.in>
2023-10-11 19:23:03 +0530

    second commit

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,10 +1,10 @@
-// function Details(Dashboard) {
-//     console.log(Dashboard.Details)
-//     return (
-//         <>
-//         <h1>user detail : {Dashboard.username}</h1>
-//         </>
-//     )
-// }
+function Details(Dashboard) {
+    console.log(Dashboard.Details)
+    return (
+        <>
+        <h1>user detail : {Dashboard.username}</h1>
+        </>
+    )
+}
 
-// export default Details;
+export default Details;
